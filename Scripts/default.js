7f67404d3033809eaa5577e5b2f78496af4b22dd
Jeff Jacobson <jacobsj@wsdot.wa.gov>
2012-09-06 16:02:35 -0700

    Added files to repository.

---
+++ Scripts/default.js
@@ -0,0 +1,19 @@
+﻿require(["esri/map", "dojo/dom", "dojo/on", "dojo/domReady!"], function (Map, dom, on, parser) {
+	"use strict";
+
+	var map, basemap;
+
+	// Create the map object
+	map = new esri.Map(dom.byId("map"));
+
+	// Add create and add a layer.
+	basemap = new esri.layers.ArcGISTiledMapServiceLayer("http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer");
+	map.addLayer(basemap);
+
+	// When the layer has loaded, add an event that will resize the map when the browser window resizes.
+	dojo.connect(map, "onLoad", function () {
+		on(window, "resize", function () {
+			map.resize();
+		});
+	});
+});
