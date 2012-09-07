659b66506b6cdbce8599d738d2242bd20637b4c5
Jeff Jacobson <jacobsj@wsdot.wa.gov>
2012-09-06 17:15:01 -0700

    Added proj4js code.

--- Scripts/default.js
+++ Scripts/default.js
@@ -1,9 +1,34 @@
 ﻿/*global require, esri, dojo*/
 /*jslint browser: true */
+
+/// <reference path="jsapi_vsdoc_v31.js" />
+/// <reference path="proj4js/proj4js-combined.js" />
+
 require(["dojo/dom", "dojo/on", "esri/map", "dojo/domReady!"], function (dom, on) {
 	"use strict";
 
 	var map, basemap;
+
+	function getProjectedPoint(point) {
+		var sourcePrj, destPrj;
+
+		// Set up the source and destination projections.
+		sourcePrj = new Proj4js.Proj('EPSG:3857');
+		destPrj = new Proj4js.Proj('EPSG:2927');
+
+		// If this is a click event, the parameter will be an event instead of a point.  Get the map point from the event.
+		if (point.mapPoint) {
+			point = point.mapPoint;
+		}
+
+		point = point.toJson();
+		Proj4js.transform(sourcePrj, destPrj, point);
+		if (point.spatialReference) {
+			point.spatialReference = new esri.SpatialReference({ wkid: 2927 });
+		}
+
+		return new esri.geometry.Point(point);
+	}
 
 	// Create the map object
 	map = new esri.Map(dom.byId("map"));
@@ -16,6 +41,13 @@ require(["dojo/dom", "dojo/on", "esri/map", "dojo/domReady!"], function (dom, on
 	dojo.connect(map, "onLoad", function () {
 		on(window, "resize", function () {
 			map.resize();
+		});
+
+		dojo.connect(map, "onClick", function (evt) {
+			var point;
+			console.debug(evt.mapPoint);
+			point = getProjectedPoint(evt.mapPoint);
+			console.debug(point);
 		});
 	});
 });
